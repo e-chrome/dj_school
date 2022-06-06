7ddaf4a87c35fa4b903e3a81560d6bcc588dc79c
Евгений Хромов <hromov.gazprom@yandex.ru>
2022-06-06 20:27:22 +0300

    initial commit

---
+++ school/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    students = Student.objects.all().order_by(ordering).prefetch_related('teacher')
+    # students = Student.objects.all().order_by(ordering)
+    context = {"students": students}
+
+    return render(request, template, context)
